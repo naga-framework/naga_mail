50be5525effdc9258850679546d85d3f7afcf718
mihawk <chan.sisowath@gmail.com>
2015-12-17 00:03:30 +0800

    refactor

--- src/naga_mail.erl
+++ src/naga_mail.erl
@@ -8,7 +8,7 @@
          handle_info/2, terminate/2, code_change/3]).
 
 -export([send/4, send/5, send/6]).
--record(state,{connection}).
+-record(state,{opts}).
 
 %% refactor from CB project, todo: send_template
 %% ---- server %% fixme: do we need?
@@ -21,41 +21,41 @@ handle_info(_, S)   -> {noreply, S}.
 
 init([]) ->
     O = options(),
-    wf:info(?SERVER,"Starting server ~p with options ~p...",[?SERVER,O]),
-    {ok, #state{connection = O}}.
+    error_logger:info_msg("Starting server ~p with options ~p...",[?SERVER,O]),
+    {ok, #state{opts=O}}.
 
-handle_call({deliver, F, T, BodyFun, ResultFun}, _From, #state{connection=C}=State) ->
+handle_call({deliver, F, T, BodyFun, ResultFun}, _From, #state{opts=C}=State) ->
     {reply, do_deliver(C, F, T, BodyFun, ResultFun), State}.
 
 %% ----- sen mail API: from,to,subject,body,args,callback
 send(F,T,S,B)     -> do_send(F,T,S,B,undefined).
 send(F,T,S,B,A)   -> send(F,T,S,B,A,undefined).
-send(F,T,S,B,A,C) -> R = io_lib:format(B,A),
-	                   do_send(F,T,S,R,C).
+send(F,T,S,B,A,C) -> do_send(F,T,S,io_lib:format(B,A),C).
 
 
 %% ---- internal
-hostname()   -> [{hostname, wf:config(naga_mail, hostname, smtp_util:guess_FQDN())}].
-ssl()        -> [{ssl, wf:config(naga_mail, ssl, false)}].
-tls()        -> [{tls, wf:config(naga_mail, mail_relay_use_tls, if_available)}].
-host()       -> case wf:config(naga_mail, mail_relay_host) of [] ->[];V ->[{relay,V}]end.
-port()       -> case wf:config(naga_mail, mail_relay_port) of [] ->[];V ->[{port,V}]end.
-credentials()-> case wf:config(naga_mail, mail_relay_username) of
-                  []       -> [{auth,never}]; 
+env(A,K)     -> env(A,K,[]).
+env(A,K,D)   -> application:get_env(A,K,D).
+hostname()   -> [{hostname, env(naga_mail, hostname, smtp_util:guess_FQDN())}].
+ssl()        -> [{ssl, env(naga_mail, ssl, false)}].
+tls()        -> [{tls, env(naga_mail, mail_relay_use_tls, if_available)}].
+host()       -> case env(naga_mail, mail_relay_host) of [] ->[];V ->[{relay,V}]end.
+port()       -> case env(naga_mail, mail_relay_port) of [] ->[];V ->[{port,V}]end.
+credentials()-> case env(naga_mail, mail_relay_username) of [] -> [{auth,never}]; 
                   Username -> [{auth,always},{username, Username},
-                               {password,wf:config(naga_mail, password, "")}] end.
-retries()    -> [{retries, wf:config(naga_mail,retries,1)}].
+                               {password,env(naga_mail, password, "")}] end.
+retries()    -> [{retries,env(naga_mail,retries,1)}].
 options()    -> ssl()++hostname()++retries()++tls()++host()++port()++credentials().
-app()        -> wf:config(naga_mail,app).
-outgoing()   -> wf:config(naga_mail,outgoing,wf:atom([app(),outgoing_mail])).
+app()        -> env(naga_mail,app).
+to_atom(A)   -> C=lists:concat([atom_to_list(A),"_outgoing_mail"]),list_to_atom(C).
+outgoing()   -> env(naga_mail,outgoing,to_atom(app())).
 mail_id()    -> (outgoing()):mail_id().
-
 
 do_deliver(Options, From, To, BodyFun, ResultFun) ->
     MailOptions = case proplists:lookup(relay, Options) of
-        none -> [_User, Host] = string:tokens(To, "@"),
-                [{relay, Host} | Options];
-        _ -> Options end,
+                    none -> [_User, Host] = string:tokens(To, "@"),
+                            [{relay, Host} | Options];
+                    _ -> Options end,
     Email = {From, [To], BodyFun},
     gen_smtp_client:send(Email, MailOptions, ResultFun).
 
@@ -63,9 +63,9 @@ do_send(F,T,S,B,C) ->
     Params = [{"Subject", S},{"To", T},{"From", F}],
     Header = build_header(Params,"text/plain",undefined),
     Fun = fun() -> [Header, "\r\n", ux2dos(B)] end,
+    gen_server:call(?SERVER, {deliver,F,T,Fun,C}).
     % O = options(),
     % do_deliver(O,F,T,Fun,C).
-    gen_server:call(?SERVER, {deliver,F,T,Fun,C}).
 
 build_header(HeaderFields, DefaultMimeType, CharSet) ->
     MessageID   = case proplists:get_value("Message-ID", HeaderFields) of
